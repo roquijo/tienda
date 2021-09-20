ef901f96b33661cc47984efb6668adc15026595e
jorge <roquijo@hotmail.com>
2021-09-20 15:51:42 -0500

    producto html en vue

--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,5 +1,178 @@
 <template>
-  
+  <html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Productos</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+<!------ Include the above in your HEAD tag ---------->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel='stylesheet' type='text/css' media='screen' href='../css/productos.css'>
+    <link rel="shortcut icon" href="../Images/comercio-electronico.png" type="image/png">
+
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="productos.html">Mercado</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent" role="navigation">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#"
+                                onclick="alert('Proyecto Desarollado Por Jorge Rojas, Gustavo Ramirez, Julian Garzon, Javier Quintero, Diego vega')">Acerca
+                                de</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true" href="login.html">Ingresar</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true" href="registro.html">Registrarse</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" style="margin-top:13px;">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Buscar">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </header>
+<!-- <div id="nav-placeholder">
+</div> -->
+
+<div class="container">
+    <h3 class="h3">Productos </h3>
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="../Images/Pc-Gamer.png">
+                    </a>
+                    <ul class="social">
+                        <li><a href="" data-tip="Especificaciones"><i class="fa fa-search"></i></a></li>
+                        <li><a href="" data-tip="Añadir a la lista de deseos"><i class="fa fa-shopping-bag"></i></a></li>
+                        <li><a href="" data-tip="Añadir al carrito"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">20%</span>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star disable"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Pc Gamer</a></h3>
+                    <div class="price">$16.00
+                        <span>$20.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="../Images/camiseta.png">
+                    </a>
+                    <ul class="social">
+                        <li><a href="" data-tip="Especificaciones"><i class="fa fa-search"></i></a></li>
+                        <li><a href="" data-tip="Añadir a la lista de deseos"><i class="fa fa-shopping-bag"></i></a></li>
+                        <li><a href="" data-tip="Añadir al carrito"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Camiseta Nike</a></h3>
+                    <div class="price">$5.00
+                        <span>$10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="../Images/zapatos.png">
+                    </a>
+                    <ul class="social">
+                        <li><a href="" data-tip="Especificaciones"><i class="fa fa-search"></i></a></li>
+                        <li><a href="" data-tip="Añadir a la lista de deseos"><i class="fa fa-shopping-bag"></i></a></li>
+                        <li><a href="" data-tip="Añadir al carrito"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Zapatillas Nike</a></h3>
+                    <div class="price">$5.00
+                        <span>$10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="product-grid">
+                <div class="product-image">
+                    <a href="#">
+                        <img class="pic-1" src="../Images/silla-gamer.png">
+                    </a>
+                    <ul class="social">
+                        <li><a href="product-details.html" data-tip="Especificaciones"><i class="fa fa-search"></i></a></li>
+                        <li><a href="" data-tip="Añadir a la lista de deseos"><i class="fa fa-shopping-bag"></i></a></li>
+                        <li><a href="" data-tip="Añadir al carrito"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                    <span class="product-new-label">Sale</span>
+                    <span class="product-discount-label">50%</span>
+                </div>
+                <ul class="rating">
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                    <li class="fa fa-star"></li>
+                </ul>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Silla Gamer</a></h3>
+                    <div class="price">$5.00
+                        <span>$10.00</span>
+                    </div>
+                    <a class="add-to-cart" href="">+ Add To Cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
+</body>
+</html>
 </template>
 
 <script>
