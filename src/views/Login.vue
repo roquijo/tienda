77463d2bf9fe01b7402e006ca09b34f6b621136c
Diego-Vega <ferzux.09@gmail.com>
2021-09-22 20:58:52 -0500

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="container mt-4">
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+  <!-- <div class="container mt-4">
     <div class="row justify-content-md-center">
       <div class="col-8">
         <form method="post">
@@ -40,11 +74,9 @@
         </form>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script></script>
 
-<style>
-
-</style>
+<style></style>
