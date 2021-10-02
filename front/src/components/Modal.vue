915bacc5f574cd35f3fd57e2cc8e9776ce435cc2
Diego-Vega <ferzux.09@gmail.com>
2021-10-02 15:36:06 -0500

    se organizo mejor el modal

---
+++ front/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-dialog transition="dialog-top-transition" max-width="600">
+    <template v-slot:activator="{ on, attrs }">
+      <a
+        class="nav-link1 active"
+        id="acercaDe"
+        v-bind="attrs"
+        v-on="on"
+      >
+        Acerca de</a
+      >
+    </template>
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-toolbar color="bg-dark" dark class="text-h4">Acerca de</v-toolbar>
+        <v-card-text>
+          <div class="text-h5 pa-6">
+            Proyecto desarrollado Por: <br />
+            Diego Vega <br />
+            Jorge Rojas <br />
+            Gustavo Jaramillo <br />
+            Javier Felipe<br />
+            Julian Garzon
+          </div>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-btn color="primary" @click="dialog.value = false">Cerrar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.nav-link1 {
+  display: block;
+  padding: 8px;
+  color: white !important;
+  text-decoration: none;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out;
+}
+</style>
--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -19,47 +19,8 @@
             >
           </li>
           <li class="nav-item">
-            <template>
-              <v-row>
-                <v-col cols="auto">
-                  <v-dialog transition="dialog-top-transition" max-width="600">
-                    <template v-slot:activator="{ on, attrs }">
-                      <a
-                        class="nav-link1 active"
-                        href="#"
-                        id="acercaDe"
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        Acerca de</a
-                      >
-                    </template>
-                    <template v-slot:default="dialog">
-                      <v-card>
-                        <v-toolbar color="bg-dark" dark class="text-h4"
-                          >Acerca de</v-toolbar
-                        >
-                        <v-card-text>
-                          <div class="text-h5 pa-6">
-                            Proyecto desarrollado Por: <br />
-                            Diego Vega <br />
-                            Jorge Rojas <br />
-                            Gustavo Jaramillo <br />
-                            Javier Felipe<br />
-                            Julian Garzon
-                          </div>
-                        </v-card-text>
-                        <v-card-actions class="justify-end">
-                          <v-btn color="primary" @click="dialog.value = false"
-                            >Cerrar</v-btn
-                          >
-                        </v-card-actions>
-                      </v-card>
-                    </template>
-                  </v-dialog>
-                </v-col>
-              </v-row>
-            </template>
+            
+            <Modal></Modal>
           </li>
         </ul>
         <form class="d-flex" style="margin-top:13px;">
@@ -77,7 +38,13 @@
 </template>
 
 <script>
-export default {};
+import Modal from "./Modal.vue";
+
+export default {
+  components: {
+    Modal,
+  },
+};
 </script>
 
 <style></style>
