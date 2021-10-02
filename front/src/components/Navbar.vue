b243d591f32d70a5c1cfe2d2ce5ccaafe4e38038
Diego-Vega <ferzux.09@gmail.com>
2021-10-02 14:28:07 -0500

    Se añadío el modal al nav bar

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -9,14 +9,6 @@
       >
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a
-              class="nav-link active"
-              href="#"
-              onclick="alert('Proyecto Desarollado Por Jorge Rojas, Gustavo Ramirez, Julian Garzon, Javier Quintero, Diego vega')"
-              >Acerca de</a
-            >
-          </li>
-          <li class="nav-item">
             <a class="nav-link active" aria-current="true" href="/ingreso"
               >Ingresar</a
             >
@@ -25,6 +17,49 @@
             <a class="nav-link active" aria-current="true" href="/registro"
               >Registrarse</a
             >
+          </li>
+          <li class="nav-item">
+            <template>
+              <v-row>
+                <v-col cols="auto">
+                  <v-dialog transition="dialog-top-transition" max-width="600">
+                    <template v-slot:activator="{ on, attrs }">
+                      <a
+                        class="nav-link1 active"
+                        href="#"
+                        id="acercaDe"
+                        v-bind="attrs"
+                        v-on="on"
+                      >
+                        Acerca de</a
+                      >
+                    </template>
+                    <template v-slot:default="dialog">
+                      <v-card>
+                        <v-toolbar color="bg-dark" dark class="text-h4"
+                          >Acerca de</v-toolbar
+                        >
+                        <v-card-text>
+                          <div class="text-h5 pa-6">
+                            Proyecto desarrollado Por: <br />
+                            Diego Vega <br />
+                            Jorge Rojas <br />
+                            Gustavo Jaramillo <br />
+                            Javier Felipe<br />
+                            Julian Garzon
+                          </div>
+                        </v-card-text>
+                        <v-card-actions class="justify-end">
+                          <v-btn color="primary" @click="dialog.value = false"
+                            >Cerrar</v-btn
+                          >
+                        </v-card-actions>
+                      </v-card>
+                    </template>
+                  </v-dialog>
+                </v-col>
+              </v-row>
+            </template>
           </li>
         </ul>
         <form class="d-flex" style="margin-top:13px;">
